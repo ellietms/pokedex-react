0531516d6bc031c9c35cef198e2703d7f4354075
ellie <ellie.tms1371@gmail.com>
2020-06-07 21:35:41 +0100

    pokedex

--- src/BestPokemon.js
+++ src/BestPokemon.js
@@ -1,20 +1,50 @@
-import React from 'react';
-import BestPokemonFetcher from './BestPokemonFetcher';
+import React,{useState,useEffect} from 'react';
+
+function BestPokemonSelector() {
+  const [pokemonId, setPokemonId] = useState(null);
+
+  function handleBulbasaurClick() {
+    setPokemonId(1);
+  }
+  function handleCharmanderClick() {
+    setPokemonId(4);
+  }
+
+  return (
+    <div>
+      <button onClick={handleBulbasaurClick}>Fetch Bulbasaur</button>
+      <button onClick={handleCharmanderClick}>Fetch Charmander</button>
+      {pokemonId ? <BestPokemonFetcher pokemonId={pokemonId} /> : null}
+    </div>
+  );
+}
+
+const BestPokemonFetcher = (props) => {
+  const [bestPokemon,setBestPokemon] = useState(null);
+  useEffect(() => {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${props.pokemonId}/`)
+      .then(response => response.json())
+      .then(data => setBestPokemon(data))
+  },[props.pokemonId])
+
+  return(
+  (!bestPokemon) ?  null : (<BestPokemon pokemon={bestPokemon} />)
+)
+} 
 
 function BestPokemon(props){
- return (
-      <div>
-      <strong> which is your favourite Pokemon?</strong>
-      <ul>
-      {props.abilities.map((ability,index) => 
-      (<li key={index}> {ability} </li>)) 
-      }
-      </ul>
-      </div>
-    );
-  
-  }
-  
- 
+  console.log(props.bestPokemon);
+  return (
+       <div>
+       <strong> which is your favourite Pokemon?</strong>
+       <ul>
+       {props.pokemon.abilities.map((element, index) => {
+        return <li key={index}>{element.ability.name}</li>;
+       })}
+       </ul>
+       </div>
+     );
+   
+}
 
-  export default BestPokemonFetcher;
+  export default BestPokemonSelector;
